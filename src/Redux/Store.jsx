d0523ad43197fbbe216b7d2cccb02db757fc4854
Vahe <vahehovakimyan574@gmail.com>
2022-08-30 14:37:50 +0400

    cf

---
+++ src/Redux/Store.jsx
@@ -0,0 +1,52 @@
+import { combineReducers,createStore } from "redux";
+
+function currentUserReducer(state = {},action) {
+    if (action.type === "edit-current-user-name") {
+        return {
+            ...state,
+            name: action.payload.name
+        };
+    }
+    return state;
+}
+
+function todosReducer(state = [],action) {
+    if (action.type === "todo-add") {
+        return {
+            ...state,
+            text: action.payload.text
+        }
+    }
+    return state;
+}
+
+const store = createStore(combineReducers({
+    currentUser: currentUserReducer,
+    todos: todosReducer
+}),{
+},{
+    posts: [],
+    friends: [],
+    todos: [
+        {
+            id: Math.random(),
+            text: "Learn Js",
+            isCompleted: false
+        },
+        {
+            id: Math.random(),
+            text: "Learn React js",
+            isCompleted: false
+        },
+        {
+            id: Math.random(),
+            text: "Learn Node js",
+            isCompleted: false
+        }
+    ],
+    currentUser: {
+        name: "Mike Tayson"
+    }
+})
+
+export default store;
