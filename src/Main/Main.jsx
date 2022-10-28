bd45f7f081ad6b57a0fee72816cf96a50fba7a32
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2022-10-28 23:24:28 +0400

    ch

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import Make_info from '../Make_info/Make_info.jsx';
-import View_info from '../View_info/View_Info.jsx';
+import Make_info from '../Make_info/Make_info';
+import View_info from '../View_info/View_Info';
 import './Main.scss';
 
 
